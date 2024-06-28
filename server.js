c98c7a78e56c0b581ce45518da3c89e85d361f36
Ahmed Adel <a7med3del1973@gmail.com>
2024-06-28 06:32:01 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+const DB =
+  'mongodb+srv://a7med3del:nodejs123@cluster0.i0ugebs.mongodb.net/natours?retryWrites=true';
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful..'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
